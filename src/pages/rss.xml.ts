8f4d5471fbd55bad1a01d77d0296fa5773abb85f
JG <jg@JGs-MacBook-Pro.home>
2024-11-11 23:59:27 +0100

    fix errors

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -21,7 +21,7 @@ export async function GET(context: Context) {
     items: items.map((item) => ({
       title: item.data.title,
       description: item.data.description,
-      pubDate: item.data.date,
+      // pubDate: item.data.date,
       link: `/${item.collection}/${item.slug}/`,
     })),
   })
