1b1f840b52fa1deb026e6bddbf9dee7fd95bfd57
JG <jg@JGs-MacBook-Pro.home>
2024-11-11 23:57:43 +0100

    fix errors

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,20 +1,18 @@
-import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
-import { HOME } from "@consts";
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
+import { HOME } from '@consts'
 
 type Context = {
   site: string
 }
 
 export async function GET(context: Context) {
-  const blog = (await getCollection("blog"))
-  .filter(post => !post.data.draft);
+  const blog = (await getCollection('blog')).filter((post) => !post.data.draft)
 
-  const projects = (await getCollection("projects"))
-    .filter(project => !project.data.draft);
+  const projects = (await getCollection('projects')).filter((project) => !project.data.draft)
 
   const items = [...blog, ...projects]
-    .sort((a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf());
+  // .sort((a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf());
 
   return rss({
     title: HOME.TITLE,
@@ -26,5 +24,5 @@ export async function GET(context: Context) {
       pubDate: item.data.date,
       link: `/${item.collection}/${item.slug}/`,
     })),
-  });
+  })
 }
